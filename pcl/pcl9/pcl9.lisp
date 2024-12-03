cb138aee33918849bec6104f1605b21d1042fb82
Anthony Le Cigne <dev@lecigne.net>
2024-12-03 21:15:03 +0100

    [pcl9] Introduce check macro

--- pcl/pcl9/pcl9.lisp
+++ pcl/pcl9/pcl9.lisp
@@ -1,7 +1,10 @@
 (defun test-+ ()
-  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
-  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -3) -4)))
+
+(defun check (form)
+  `(report-result ,form ',form))
 
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
