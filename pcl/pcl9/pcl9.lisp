95ebf33d2583eec80cff593d32902c829debd09a
Anthony Le Cigne <dev@lecigne.net>
2024-12-03 22:03:11 +0100

    [pcl9] Introduce combine-results

--- pcl/pcl9/pcl9.lisp
+++ pcl/pcl9/pcl9.lisp
@@ -1,3 +1,7 @@
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
 (defun test-+ ()
   (check
     (= (+ 1 2) 3)
@@ -5,7 +9,15 @@
     (= (+ -1 -3) -4)))
 
 (defmacro check (&body forms)
-  `(progn ,@(loop for f in forms collect `(report-result ,f ',f))))
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
