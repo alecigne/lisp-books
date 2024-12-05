e305e8eb4079453731acbd2d27a7c6a0ca59cee5
Anthony Le Cigne <dev@lecigne.net>
2024-12-05 19:19:18 +0100

    [pcl9] Add deftest macro and use it

--- pcl/pcl9/pcl9.lisp
+++ pcl/pcl9/pcl9.lisp
@@ -4,27 +4,9 @@
 
 (defvar *test-name* nil)
 
-(defun test-+ ()
-  (let ((*test-name* 'test-+))
-    (check
-      (= (+ 1 2) 3)
-      (= (+ 1 2 3) 6)
-      (= (+ -1 -3) -4))))
-
-(defun test-* ()
-  (let ((*test-name* 'test-*))
-    (check
-      (= (* 2 2) 4)
-      (= (* 3 5) 15))))
-
-(defun test-arithmetic ()
-  (combine-results
-    (test-+)
-    (test-*)))
-
-(defmacro check (&body forms)
-  `(combine-results
-     ,@(loop for f in forms collect `(report-result ,f ',f))))
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
 
 (defmacro combine-results (&body forms)
   (with-gensyms (result)
@@ -32,6 +14,32 @@
        ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
        ,result)))
 
-(defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
-  result)
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro deftest (name params &body body)
+  `(defun ,name ,params
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+;; Testing
+
+(deftest test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+(deftest test-* ()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 5) 15)))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(deftest test-math ()
+  (test-arithmetic))
